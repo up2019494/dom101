4b04f16f2b9fbc6890e60d1547fb5f3032203b8a
ear1grey <rjb@port.ac.uk>
2016-10-11 13:37:15 +0100

    reverse list

--- assess.dom.js
+++ assess.dom.js
@@ -33,3 +33,45 @@ QUnit.test("testMutateMainUsingDocument",
         console.log();
     }
 );
+
+
+
+
+QUnit.test("reverseList",
+    function(assert) {
+
+        assert.ok(
+            typeof reverseList === "function",
+            "Write a function `reverseList` that can reverse the content of a list.  The function should take one parameter which is a selector, used to choose which list is reversed."
+        );
+
+        assert.strictEqual(
+          window.reverseme.firstElementChild.textContent,
+          "1",
+          "Before running, first element is 1."
+        );
+
+        reverseList("#reverseme");
+
+        assert.strictEqual(
+          window.reverseme.firstElementChild.textContent,
+          "5",
+          "After running, first element must be 5."
+        );
+
+        assert.strictEqual(
+          window.reverseme.lastElementChild.textContent,
+          "1",
+          "After running, last element must be 1."
+        );
+
+        reverseList("#reverseme");
+
+        assert.strictEqual(
+          window.reverseme.firstElementChild.textContent,
+          "1",
+          "After running again, first element is 1."
+        );
+
+    }
+);
