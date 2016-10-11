d14c8d53fec9c3ddee82b21d15a24b9ff21de11a
ear1grey <rjb@port.ac.uk>
2016-10-11 20:14:13 +0100

    last 4 written, minor tweaks to HTM

--- assess.dom.js
+++ assess.dom.js
@@ -13,14 +13,15 @@ QUnit.test("testMutateMainUsingDocument",
         mutateMain();
 
         assert.strictEqual(
-          main.firstElementChild.tagName,
+          window.main.firstElementChild.tagName,
           "P",
           "Use document.createElement to create a new paragraph element.  Append this paragraph to the `main` element."
         );
 
+
         assert.strictEqual(
           document.getElementById("example1"),
-          main.firstElementChild,
+          window.main.firstElementChild,
           "The element you create should be given the id attribute of `example1`."
         );
 
@@ -30,7 +31,7 @@ QUnit.test("testMutateMainUsingDocument",
           "hello world",
           "Set the text of the `example1` paragraph element to be `hello world`."
         );
-        console.log();
+        window.console.log();
     }
 );
 
@@ -72,6 +73,100 @@ QUnit.test("reverseList",
           "1",
           "After running again, first element is 1."
         );
+
+    }
+);
+
+
+QUnit.test("listMover",
+    function(assert) {
+
+        assert.ok(
+            typeof mover === "function",
+            "Write a function `mover` that accepts two parameters, the first is a selector for the element to move, the second is a selector for the elenebt that it should be appended to."
+        );
+
+        assert.ok(
+          window.moveme.parentElement == window.listone,
+          "Before, moveme has listone as a parent."
+        );
+
+
+        assert.strictEqual(
+          window.listone.children.length,
+          4,
+          "Before running, list one has 4 elements."
+        );
+
+        assert.strictEqual(
+          window.listtwo.children.length,
+          3,
+          "Before running, list one has 3 elements."
+        );
+
+        mover( "#moveme", "#listtwo");
+
+
+        assert.strictEqual(
+          window.listone.children.length,
+          3,
+          "After running, list one has 3 elements."
+        );
+
+        assert.strictEqual(
+          window.listtwo.children.length,
+          4,
+          "Before running, list one has 4 elements."
+        );
+
+        assert.ok(
+          window.moveme.parentElement == window.listtwo,
+          "After, moveme has a new parent."
+        );
+
+    }
+);
+
+
+
+QUnit.test("duplication",
+    function(assert) {
+
+        var thingToBeDuped = window.dupeme;
+
+        assert.ok(
+            typeof dupe === "function",
+            "Write a function `dupe` that accepts one parameter, a selector, The function should duplicate."
+        );
+
+        assert.strictEqual(
+          thingToBeDuped.parentElement.children.length,
+          2,
+          "The parent of #dupeme should have just two children before the test run, a heading and #dupeme."
+        );
+
+        dupe("#dupeme");
+
+        assert.strictEqual(
+          thingToBeDuped.parentElement.children.length,
+          3,
+          "The parent of #dupeme should have three children after the test run, a heading and #dupeme AND the duplicate."
+        );
+
+        var paras = thingToBeDuped.parentElement.querySelectorAll("p");
+
+        assert.strictEqual(
+          paras.length,
+          2,
+          "There should be two paras after the test."
+        );
+
+        assert.strictEqual(
+          paras[0].textContent,
+          paras[1].textContent,
+          "The two paragraphs should have the same text."
+        );
+
 
     }
 );
