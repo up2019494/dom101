84e66617728f9be1cef4941669c6cae36af61a1c
Jacek Kopecky <jacek@jacek.cz>
2016-10-12 00:09:00 +0100

    remove old placeholder test

--- assess.dom.js
+++ assess.dom.js
@@ -2,50 +2,6 @@
 
 QUnit.module("DOM");
 
-QUnit.test("testMutateMainUsingDocument",
-    function(assert) {
-
-        assert.ok(
-            typeof mutateMain === "function",
-            "Create a function called `mutateMain` that changes the `main` element within the DOM and thus allows all of the assertions in this test to pass."
-        );
-
-        mutateMain();
-
-        var main = document.querySelector("main");
-
-
-        assert.ok(
-          main,
-          "There should be a main element in the document."
-        );
-
-        assert.strictEqual(
-          main.children[0].tagName,
-          "P",
-          "Use document.createElement to create a new paragraph element.  Append this paragraph to the `main` element."
-        );
-
-
-        assert.strictEqual(
-          document.getElementById("example1"),
-          main.firstElementChild,
-          "The element you create should be given the id attribute of `example1`."
-        );
-
-
-        assert.strictEqual(
-          document.getElementById("example1").textContent,
-          "hello world",
-          "Set the text of the `example1` paragraph element to be `hello world`."
-        );
-        window.console.log();
-    }
-);
-
-
-
-
 QUnit.test("reverseList",
     function(assert) {
 
