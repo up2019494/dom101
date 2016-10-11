cd5e93dd90abde3a7017c65470aec27bea6b7b64
ear1grey <rjb@port.ac.uk>
2016-10-11 22:04:34 +0100

    for reference when developing

--- index.js
+++ index.js
@@ -31,3 +31,39 @@ function mutateMain() {
   p.innerText = "hello world";
   main.appendChild(p);
 }
+
+function reverseList(selector) {
+  var list = document.querySelector(selector);
+  var items = [];
+  var i;
+  for (i = 0; i < list.children.length; i++) {
+    items.unshift(list.children[i]);
+  }
+  for (i = 0; i < list.children.length; i++) {
+    list.appendChild(items[i]);
+  }
+}
+
+
+function mover(whatToMove, whereToMove) {
+  var moveThisThing = document.querySelector(whatToMove);
+  var moveToHere = document.querySelector(whereToMove);
+  moveToHere.appendChild(moveThisThing);
+}
+
+
+function dupe(what) {
+  var dupeThis = document.querySelector(what);
+  var duped = dupeThis.cloneNode(true);
+  if (duped.id) {
+    duped.id += "_";
+  }
+  dupeThis.parentElement.insertBefore(duped, dupeThis);
+}
+
+function removeAll(selector) {
+  var removables = document.querySelectorAll(selector);
+  for (var i = 0; i < removables.length; i++) {
+    removables[i].parentElement.removeChild(removables[i]);
+  }
+}
