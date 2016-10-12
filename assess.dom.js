8849852dc27b20f2779d2406d2d8aaf4712f1ecb
ear1grey <rjb@port.ac.uk>
2016-10-12 17:01:42 +0100

    :art::poop:

--- assess.dom.js
+++ assess.dom.js
@@ -5,7 +5,7 @@ QUnit.module("DOM");
 
 // turns an array-like into an array
 function arr(a) {
-  return Array.prototype.slice.call(a);
+    return Array.prototype.slice.call(a);
 }
 
 
@@ -82,36 +82,36 @@ QUnit.test("setAttributes",
         );
 
         assert.strictEqual(
-          document.getElementById('animals').src,
-          "http://placekitten.com/400/200"
+            document.getElementById('animals').src,
+            "http://placekitten.com/400/200"
         );
 
         assert.strictEqual(
-          document.getElementById('animals').alt,
-          "A kitten."
+            document.getElementById('animals').alt,
+            "A kitten."
         );
 
         assert.strictEqual(
-          document.getElementById('animals').alt,
-          "A kitten."
+            document.getElementById('animals').alt,
+            "A kitten."
         );
 
 
         moreBears();
 
         assert.strictEqual(
-          document.getElementById('animals').src,
-          "http://placebear.com/400/200"
+            document.getElementById('animals').src,
+            "http://placebear.com/400/200"
         );
 
         assert.strictEqual(
-          document.getElementById('animals').alt,
-          "A bear."
+            document.getElementById('animals').alt,
+            "A bear."
         );
 
         assert.strictEqual(
-          document.getElementById('animals').title,
-          "A BEAR!"
+            document.getElementById('animals').title,
+            "A BEAR!"
         );
 
         document.getElementById('animals').parentElement.classList.add("done");
@@ -130,29 +130,29 @@ QUnit.test("setTheId",
 
         var p = document.createElement("p");
 
-      assert.notOk(
-          p.id,
-          "Before running, p has no Id."
+        assert.notOk(
+            p.id,
+            "Before running, p has no Id."
         );
 
         var q = setId(p, "lalala");
 
         assert.strictEqual(
-          q,
-          p,
-          "The function must return its element parameter."
+            q,
+            p,
+            "The function must return its element parameter."
         );
 
         assert.strictEqual(
-          p.id,
-          "lalala"
+            p.id,
+            "lalala"
         );
 
         setId(p, "foobarbaz");
 
         assert.strictEqual(
-          p.id,
-          "foobarbaz"
+            p.id,
+            "foobarbaz"
         );
     }
 );
@@ -170,27 +170,27 @@ QUnit.test("setTheClass",
         var p = document.getElementById("setmyclass");
 
         assert.notOk(
-          p.className
+            p.className
         );
 
         var q = setClass(p, "banal");
 
         assert.strictEqual(
-          q,
-          p,
-          "The function must return its element parameter."
+            q,
+            p,
+            "The function must return its element parameter."
         );
 
         assert.strictEqual(
-          p.className,
-          "banal"
+            p.className,
+            "banal"
         );
 
         setClass(p, "interesting");
 
         assert.strictEqual(
-          p.className,
-          "interesting"
+            p.className,
+            "interesting"
         );
 
         window.setmyclass.parentElement.classList.add("done");
@@ -209,46 +209,46 @@ QUnit.test("addAClass",
         var p = document.getElementById("wantaborder");
 
         assert.strictEqual(
-          p.classList.length,
-          1
+            p.classList.length,
+            1
         );
 
         assert.strictEqual(
-          p.className,
-          "greatcontent"
+            p.className,
+            "greatcontent"
         );
 
         var q = addAClass(p, "excellence");
 
         assert.strictEqual(
-          q,
-          p,
-          "The function must return its element parameter."
+            q,
+            p,
+            "The function must return its element parameter."
         );
 
 
         assert.strictEqual(
-          p.classList.length,
-          2
+            p.classList.length,
+            2
         );
 
         assert.ok(
-          p.classList.contains("excellence")
+            p.classList.contains("excellence")
         );
 
         addAClass(p, "lovely");
 
         assert.strictEqual(
-          p.classList.length,
-          3
+            p.classList.length,
+            3
         );
 
         assert.ok(
-          p.classList.contains("excellence")
+            p.classList.contains("excellence")
         );
 
         assert.ok(
-          p.classList.contains("lovely")
+            p.classList.contains("lovely")
         );
 
         window.wantaborder.parentElement.classList.add("done");
@@ -269,25 +269,25 @@ QUnit.test("removeAClass",
         var p = document.getElementById("helpme");
 
         assert.strictEqual(
-          p.classList.length,
-          3
+            p.classList.length,
+            3
         );
 
 
         assert.ok(
-          p.classList.contains("downer"),
-          "Before running, the array of classes contains `downer`."
+            p.classList.contains("downer"),
+            "Before running, the array of classes contains `downer`."
         );
 
         removeAClass(p, "downer");
 
         assert.notOk(
-          p.classList.contains("downer")
+            p.classList.contains("downer")
         );
 
         assert.strictEqual(
-          p.classList.length,
-          2
+            p.classList.length,
+            2
         );
 
         p.parentElement.classList.add("done");
@@ -376,8 +376,7 @@ QUnit.test("findElementByQuery",
         );
 
         assert.deepEqual(
-            arr(findElementByQuery('#thisisaparagraph')),
-            [ window.thisisaparagraph ]
+            arr(findElementByQuery('#thisisaparagraph')), [window.thisisaparagraph]
         );
 
 
@@ -392,8 +391,7 @@ QUnit.test("findElementByQuery",
         );
 
         assert.deepEqual(
-            arr(findElementByQuery('mustnotexist')),
-            []
+            arr(findElementByQuery('mustnotexist')), []
         );
     }
 );
@@ -410,38 +408,38 @@ QUnit.test("reverseList",
         );
 
         assert.strictEqual(
-          window.reverseme.firstElementChild.textContent,
-          "1",
-          "Before running, first element is 1."
+            window.reverseme.firstElementChild.textContent,
+            "1",
+            "Before running, first element is 1."
         );
 
         reverseList("#reverseme");
 
         assert.strictEqual(
-          window.reverseme.firstElementChild.textContent,
-          "5",
-          "After running, first element must be 5."
+            window.reverseme.firstElementChild.textContent,
+            "5",
+            "After running, first element must be 5."
         );
 
         assert.strictEqual(
-          window.reverseme.lastElementChild.textContent,
-          "1",
-          "After running, last element must be 1."
+            window.reverseme.lastElementChild.textContent,
+            "1",
+            "After running, last element must be 1."
         );
 
         // reverse another list
         var returnedList = reverseList("#reversemetoo");
 
         assert.strictEqual(
-          window.reversemetoo.firstElementChild.textContent,
-          "The Force Awakens",
-          "After running again, first element is The Force Awakens."
+            window.reversemetoo.firstElementChild.textContent,
+            "The Force Awakens",
+            "After running again, first element is The Force Awakens."
         );
 
         assert.strictEqual(
-          window.reversemetoo,
-          returnedList,
-          "The selected element is returned."
+            window.reversemetoo,
+            returnedList,
+            "The selected element is returned."
         );
 
         window.reverseme.parentElement.classList.add("done");
@@ -459,74 +457,74 @@ QUnit.test("listMover",
         );
 
         assert.ok(
-          window.moveme.parentElement == window.listone,
-          "Before, `moveme` has `listone` as a parent."
+            window.moveme.parentElement == window.listone,
+            "Before, `moveme` has `listone` as a parent."
         );
 
 
         assert.strictEqual(
-          window.listone.children.length,
-          4,
-          "Before running, list one has 4 elements."
+            window.listone.children.length,
+            4,
+            "Before running, list one has 4 elements."
         );
 
         assert.strictEqual(
-          window.listtwo.children.length,
-          3,
-          "Before running, list two has 3 elements."
+            window.listtwo.children.length,
+            3,
+            "Before running, list two has 3 elements."
         );
 
-        mover( "#moveme", "#listtwo")
+        mover("#moveme", "#listtwo")
 
         assert.strictEqual(
-          window.listone.children.length,
-          3,
-          "After running, list one has 3 elements."
+            window.listone.children.length,
+            3,
+            "After running, list one has 3 elements."
         );
 
         assert.strictEqual(
-          window.listtwo.children.length,
-          4,
-          "After running, list two has 4 elements."
+            window.listtwo.children.length,
+            4,
+            "After running, list two has 4 elements."
         );
 
         assert.ok(
-          window.moveme.parentElement == window.listtwo,
-          "After, moveme has a new parent."
+            window.moveme.parentElement == window.listtwo,
+            "After, moveme has a new parent."
         );
 
-        mover( "#moveme", "#listone")
+        mover("#moveme", "#listone")
 
         assert.strictEqual(
-          window.listone.children.length,
-          4,
-          "After running again, list one has 4 elements."
-        );
-
-        assert.strictEqual(
-          window.listtwo.children.length,
-          3,
-          "Before running again, list one has 3 elements."
-        );
-
-
-        mover( "#moveme", "#listone");
-
-        assert.strictEqual(
-          window.listone.children.length,
-          4,
-          "After running, list one has 4 elements."
+            window.listone.children.length,
+            4,
+            "After running again, list one has 4 elements."
         );
 
         assert.strictEqual(
-          window.listtwo.children.length,
-          3,
-          "After running, list two has 3 elements."
+            window.listtwo.children.length,
+            3,
+            "Before running again, list one has 3 elements."
+        );
+
+
+        mover("#moveme", "#listone");
+
+        assert.strictEqual(
+            window.listone.children.length,
+            4,
+            "After running, list one has 4 elements."
+        );
+
+        assert.strictEqual(
+            window.listtwo.children.length,
+            3,
+            "After running, list two has 3 elements."
         );
 
         assert.ok(
-          window.moveme.parentElement == window.listone,
-          "After, moveme has a new parent."
+            window.moveme.parentElement == window.listone,
+            "After, moveme has a new parent."
         );
 
 
@@ -553,33 +551,33 @@ QUnit.test("listFiller",
         filler(ul, entries)
 
         for (var i = 0; i < ul.children.length; i++) {
-          assert.strictEqual(
-            ul.children[i].textContent,
-            entries[i],
-            "Text in each element should match the text in the array."
-          );
+            assert.strictEqual(
+                ul.children[i].textContent,
+                entries[i],
+                "Text in each element should match the text in the array."
+            );
         }
 
         assert.strictEqual(
-          ul.children.length,
-          3,
-          "There should be exactly three list items."
+            ul.children.length,
+            3,
+            "There should be exactly three list items."
         );
 
 
         var unshowntest = document.createElement("ul");
         filler(unshowntest, []);
         assert.strictEqual(
-          unshowntest.children.length,
-          0,
-          "There should be exactly zero list items if called with an empty array."
+            unshowntest.children.length,
+            0,
+            "There should be exactly zero list items if called with an empty array."
         );
 
         filler(unshowntest, ["Sloane", "Kyrell", "Wexley", "Rax"]);
         assert.strictEqual(
-          unshowntest.children.length,
-          4,
-          "There should be exactly four list items if called with an array of four items."
+            unshowntest.children.length,
+            4,
+            "There should be exactly four list items if called with an array of four items."
         );
 
 
@@ -600,31 +598,31 @@ QUnit.test("duplication",
         );
 
         assert.strictEqual(
-          thingToBeDuped.parentElement.children.length,
-          2,
-          "The parent of #dupeme should have just two children before the test run, a heading and #dupeme."
+            thingToBeDuped.parentElement.children.length,
+            2,
+            "The parent of #dupeme should have just two children before the test run, a heading and #dupeme."
         );
 
         dupe("#dupeme");
 
         assert.strictEqual(
-          thingToBeDuped.parentElement.children.length,
-          3,
-          "The parent of #dupeme should have three children after the test run, a heading and #dupeme AND the duplicate."
+            thingToBeDuped.parentElement.children.length,
+            3,
+            "The parent of #dupeme should have three children after the test run, a heading and #dupeme AND the duplicate."
         );
 
         var paras = thingToBeDuped.parentElement.querySelectorAll("p");
 
         assert.strictEqual(
-          paras.length,
-          2,
-          "There should be two paras after the test."
+            paras.length,
+            2,
+            "There should be two paras after the test."
         );
 
         assert.strictEqual(
-          paras[0].textContent,
-          paras[1].textContent,
-          "The two paragraphs should have the same text."
+            paras[0].textContent,
+            paras[1].textContent,
+            "The two paragraphs should have the same text."
         );
 
 
@@ -650,20 +648,20 @@ QUnit.test("removeAllMatchingNodes",
         var paras = document.querySelectorAll("#noevens p");
 
         assert.strictEqual(
-          paras.length,
-          3,
-          "There should be three paras after the test."
+            paras.length,
+            3,
+            "There should be three paras after the test."
         );
 
         for (var i = 0; i < paras.length; i++) {
-          assert.strictEqual(
-            paras[i].textContent,
-            "I love you",
-            "The remaining text should only say I love you."
-          );
-      }
+            assert.strictEqual(
+                paras[i].textContent,
+                "I love you",
+                "The remaining text should only say I love you."
+            );
+        }
 
-      parent.classList.add("done");
+        parent.classList.add("done");
     }
 );
 
@@ -678,8 +676,11 @@ QUnit.test("form elements",
         var user = getUserData();
 
         assert.deepEqual(
-            user,
-            { name: 'John', speed: 70, student: true }
+            user, {
+                name: 'John',
+                speed: 70,
+                student: true
+            }
         )
 
         window.username.value = "Jane";
@@ -689,8 +690,11 @@ QUnit.test("form elements",
         var user = getUserData();
 
         assert.deepEqual(
-            user,
-            { name: 'Jane', speed: 100, student: false }
+            user, {
+                name: 'Jane',
+                speed: 100,
+                student: false
+            }
         )
         window.username.parentElement.parentElement.classList.add('done');
     }
