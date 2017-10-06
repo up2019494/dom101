9548aaf85e08cc0155f39e156e3056737d196809
Jacek Kopecky <jacek@jacek.cz>
2017-10-06 13:34:49 +0100

    update basics from last week

--- assess.basics.js
+++ assess.basics.js
@@ -1,11 +1,18 @@
-"use strict";
 QUnit.module("Basics");
 
 QUnit.test("Change the id method in index.js to return your student ID.",
     function(assert) {
+        assert.ok(
+            id(),
+            "The function must return something."
+        );
         assert.notOk(
             id() == "Replace This With Your Student ID",
             "This test will pass as soon as the id function returns anything other than the default value.  You should make sure it returns your student ID!"
+        );
+        assert.notOk(
+            id() == "UP654321",
+            "This second test will pass as soon as the id function returns anything other than the other default value."
         );
     }
 );
@@ -13,6 +20,10 @@ QUnit.test("Change the id method in index.js to return your student ID.",
 
 QUnit.test("Change the fn method in index.js to return your first name.",
     function(assert) {
+        assert.ok(
+            fn(),
+            "The function must return something."
+        );
         assert.notOk(
             fn() == "Replace This With Your First Name",
             "This test will pass as soon as the fn function returns anything other than the default value.  You should make sure it returns your student first name!"
@@ -22,6 +33,10 @@ QUnit.test("Change the fn method in index.js to return your first name.",
 
 QUnit.test("Change the sn method in index.js to return your surname (family name).",
     function(assert) {
+        assert.ok(
+            sn(),
+            "The function must return something."
+        );
         assert.notOk(
             sn() == "Replace This With Your Surname",
             "This test will pass as soon as the sn function returns anything other than the default value.  You should make sure it returns your surname!"
